863543b5f0e79dff11e0d246de987815075d2712
maldata <maldata@gmail.com>
2020-12-12 00:54:44 -0600

    Added the solution for part 2020 day 10 part b. I don't love the algorithm.

--- 2020/10/solve-b.py
+++ 2020/10/solve-b.py
@@ -1,14 +1,4 @@
 #!/usr/bin/env python3
-def count_paths(joltages):
-    if len(joltages) == 1:
-        return 1
-
-    valid_next_joltages = filter(lambda x: x <= joltages[0] + 3, joltages[1:])
-    child_indices = [joltages.index(i) for i in valid_next_joltages]
-    child_paths = [count_paths(joltages[c:]) for c in child_indices]
-    return sum(child_paths)
-
-
 def main():
     with open('./input.txt', 'r') as f:
         all_lines = f.readlines()
@@ -19,8 +9,31 @@ def main():
     # Add the wall joltage and the built-in adapter that's 3 jolts higher than the maximum
     all_joltages = [0] + all_adapters + [all_adapters[-1] + 3]
 
-    # We need to count the number of valid paths from the wall joltage to the device joltage
-    num_paths = count_paths(all_joltages)
+    # We need to count the number of valid paths from the wall joltage to the device joltage.
+    # Number of one-gaps between three-gaps        Number of paths between
+    #                                     0        1
+    #                                     1        1
+    #                                     2        2
+    #                                     3        4
+    #                                     4        7
+    gap_map = {0: 1, 1: 1, 2: 2, 3: 4, 4: 7}
+    gaps = list([i[1]-i[0] for i in zip(all_joltages[:-1], all_joltages[1:])])
+
+    runs_of_ones = []
+    current_run_length = 0
+    for idx in range(len(gaps)):
+        if gaps[idx] == 3:
+            runs_of_ones.append(current_run_length)
+            current_run_length = 0
+        else:
+            current_run_length = current_run_length + 1
+
+    multipliers = [gap_map[i] for i in runs_of_ones]
+
+    num_paths = 1
+    for i in multipliers:
+        num_paths = num_paths * i
+    
     print('There are {0} valid arrangements of adapters.'.format(num_paths))
 
 
