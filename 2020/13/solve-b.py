16fcce6595c685728a6e14564da3c1691b48bd4e
maldata <maldata@gmail.com>
2020-12-15 22:09:10 -0600

    Finished the sieve solution for 2020 day 13 part b

--- 2020/13/solve-b.py
+++ 2020/13/solve-b.py
@@ -21,14 +21,33 @@ def main():
     current_time = current_time - offset
 
     # Now we just go minute by minute until we find the time when each bus
-    # comes in offset by the number of minutes in the time_offsets list
+    # comes in offset by the number of minutes in the time_offsets list.
+    # This will take forever, so we do a sieve-type thing. We'll find when
+    # the first two are in the right position, and then get the period at
+    # which those two are aligned. Then we'll steps at that period until the
+    # third one is aligned, and so on.
 
+    step_size = first_bus_id
+    bus_subset_idx = 2
+    bus_subset = int_buses[0:bus_subset_idx]
     while True:
-        moduli = [(current_time + time_offsets[i]) % int_buses[i] for i in range(len(int_buses))]
+        moduli = [(current_time + time_offsets[i]) % int_buses[i] for i in range(len(bus_subset))]
         mod0 = [m == 0 for m in moduli]
         if all(mod0):
-            break
-        current_time = current_time + first_bus_id
+            # If all the buses in the subset line up, we'll get here.
+            
+            # If we've included all of the buses, we're done!
+            if len(bus_subset) == len(int_buses):
+                break
+            
+            # If there are more buses, we increase the step size, then we add another
+            # to the subset and we move on. Note that we don't change the current_time,
+            # though... we'll want to check that time with the larger subset.
+            step_size = step_size * int_buses[bus_subset_idx - 1]
+            bus_subset_idx = bus_subset_idx + 1
+            bus_subset = int_buses[0:bus_subset_idx]
+            continue
+        current_time = current_time + step_size
 
     print('The buses line up sequentially by minute starting at time {0}'.format(current_time))
 
