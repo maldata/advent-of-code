130a89aade7335f3887b387b86139ec0677b2f59
Mark Locascio <markl@dmcinfo.com>
2020-12-22 15:35:35 -0600

    Finished 2020 day 18 part a

--- 2020/18/solve-a.py
+++ 2020/18/solve-a.py
@@ -26,7 +26,41 @@ def get_next_operator(expr):
     return operator, remaining_expr
 
 
+def find_innermost_parenthetical(expr):
+    """
+    Given the expression expr, find the innermost parenthetical expression.
+    Return a tuple containing the index and length.
+    """
+    # First find a closing parenthesis
+    close_paren_idx = expr.index(')')
+
+    # From there, backtrack until you find the nearest opening parenthesis before it
+    open_offset = expr[:close_paren_idx][::-1].index('(')
+    open_idx = close_paren_idx - open_offset - 1
+    length = close_paren_idx - open_idx + 1
+
+    return open_idx, length
+
+
+def replace_parenthetical(expr, index, length):
+    pre = expr[:index]
+    post = expr[index + length:]
+
+    parenthetical = expr[index:index + length]
+
+    # Strip off the enclosing parentheses
+    contents = parenthetical.strip('()')
+
+    flattened = pre + str(flat_evaluate(contents)) + post
+    return flattened
+
+
 def flatten_expr(expr):
+    expr = expr.strip()
+    while expr.count('(') != 0:
+        paren = find_innermost_parenthetical(expr)
+        expr = replace_parenthetical(expr, paren[0], paren[1])
+
     return expr
 
 
@@ -53,15 +87,18 @@ def flat_evaluate(expr):
 
 
 def main():
-    with open('./test-input1.txt', 'r') as f:
-        expr = f.readlines()
+    with open('./input.txt', 'r') as f:
+        all_lines = f.readlines()
 
-    expr = expr[0]
-    expr = expr.strip()
+    expressions = [i.strip() for i in all_lines]
 
-    flattened_expr = flatten_expr(expr)
-    result = flat_evaluate(flattened_expr)
-    print(result)
+    sum_of_exprs = 0
+    for expr in expressions:
+        flattened_expr = flatten_expr(expr)
+        result = flat_evaluate(flattened_expr)
+        sum_of_exprs = sum_of_exprs + result
+
+    print(sum_of_exprs)
 
 
 if __name__ == '__main__':
