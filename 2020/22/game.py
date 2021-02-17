d273613892f332cb8e95294403447f9980587b0b
maldata <maldata@gmail.com>
2021-02-16 21:33:54 -0600

    Finished 2020 day 22 part b

--- 2020/22/game.py
+++ 2020/22/game.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
+import pdb
+
 class Game:
     def __init__(self, deck0, deck1):
-        self._initial_deck0 = list(deck0)
-        self._initial_deck1 = list(deck1)
         self._deck0 = list(deck0)
         self._deck1 = list(deck1)
         self._history = []
@@ -16,13 +16,20 @@ class Game:
 
     def get_winner_index(self):
         if self.game_finished():
-            return 0 if len(self._deck1) > 0 else 1
+            return 0 if len(self._deck0) > 0 else 1
         else:
             return None
-    
+        
     def get_scores(self):
-        # TODO: get the scores based on the current decks
-        return (0, 0)
+        scores = []
+        for deck in [self._deck0, self._deck1]:
+            points_per_card = list(range(1, len(deck) + 1))
+            points_per_card.reverse()
+            z = zip(deck, points_per_card)
+            score = sum([a * b for a, b in z])
+            scores.append(score)
+        
+        return scores
     
     def play(self):
         """
@@ -30,8 +37,13 @@ class Game:
         then return the index of the winner.
         """
         while not self.game_finished():
-            # TODO: if the current decks are in the history, return 0.
-            round_winner = self.do_one_round()
+            deck_state = (list(self._deck0), list(self._deck1))
+            if deck_state in self._history:
+                return 0
+            
+            self._history.append(deck_state)
+            
+            round_winner = self.get_round_winner()
 
             p0 = self._deck0[0]
             p1 = self._deck1[0]
@@ -46,14 +58,15 @@ class Game:
                 print('The winner of a round was None!!!')
                 break
 
-        return 0 if len(self._deck1) == 0 else 1
+        return self.get_winner_index()
 
-    def do_one_round(self):
+    def get_round_winner(self):
         """
         Determine the winner of one round. That is, draw a card from each
         deck and do a recursive subgame or determine the winner based on
-        the drawn card. This method returns the index of the winner and also
-        puts the drawn cards into the winner's deck.
+        the drawn card. This method returns the index of the winner but
+        does not remove a card from the front of each deck or put cards
+        into the winner's deck.
         """
         p0 = self._deck0[0]
         p1 = self._deck1[0]
