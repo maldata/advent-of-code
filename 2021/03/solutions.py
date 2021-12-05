e327150cab8ef3f4b22ce7b261693c66a78a7f08
maldata <maldata@gmail.com>
2021-12-04 23:43:57 -0600

    Finished 2021 day 3 part b (python)

--- 2021/03/solutions.py
+++ 2021/03/solutions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-def read_report():
-    with open('./input.txt', 'r') as f:
+def read_report(file_path):
+    with open(file_path, 'r') as f:
         all_lines = f.readlines()
 
     stripped_lines = [line.strip() for line in all_lines]
@@ -11,37 +11,35 @@ def read_report():
 def get_most_common(report):
     num_lines = len(report)
     bits = list(zip(*report))
-    sums = [sum(b) for b in bits]
-    return [0 if s <= num_lines / 2 else 1 for s in sums]
+    num_ones = [sum(b) for b in bits]
+    num_zeros = [num_lines - n for n in num_ones]
+
+    return [0 if num_zeros[i] > num_ones[i] else 1 for i in range(len(num_ones))]
 
 
 def get_least_common(report):
-    most_common_bits = get_most_common(report)
-    return [-i + 1 for i in most_common_bits]
+    num_lines = len(report)
+    bits = list(zip(*report))
+    num_ones = [sum(b) for b in bits]
+    num_zeros = [num_lines - n for n in num_ones]
+
+    return [1 if num_zeros[i] > num_ones[i] else 0 for i in range(len(num_ones))]
 
 
 def binary_list_to_integer(input_list):
+    list_copy = [i for i in input_list]
+    list_copy.reverse()
     decimal_num = 0
-    for i in range(len(input_list)):
-        decimal_num = decimal_num + input_list[i] * (2 ** i)    
+    for i in range(len(list_copy)):
+        decimal_num = decimal_num + list_copy[i] * (2 ** i)    
     return decimal_num
 
 
-def get_gamma(report):
-    most_common_bits = get_most_common(report)
-    most_common_bits.reverse()
-    return binary_list_to_integer(most_common_bits)
-    
-
-def get_epsilon(report):
-    least_common_bits = get_least_common(report)
-    least_common_bits.reverse()
-    return binary_list_to_integer(least_common_bits)
-
-
 def solve_a(report):
-    gamma = get_gamma(report)
-    epsilon = get_epsilon(report)
+    most_common_bits = get_most_common(report)
+    gamma = binary_list_to_integer(most_common_bits)
+    least_common_bits = get_least_common(report)
+    epsilon = binary_list_to_integer(least_common_bits)
     print('Power consumption: {0}'.format(gamma * epsilon))
 
 
@@ -74,7 +72,7 @@ def solve_b(report):
 
 
 def main():
-    report = read_report()
+    report = read_report('./input.txt')
     solve_a(report)
     solve_b(report)
 
