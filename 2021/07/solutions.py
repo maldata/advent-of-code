be886d35c4df084acf8f9f240ffdb55f00ab49eb
maldata <maldata@gmail.com>
2021-12-07 22:25:18 -0600

    Added 2021 day 7 part b in python

--- 2021/07/solutions.py
+++ 2021/07/solutions.py
@@ -1,3 +1,5 @@
+from math import floor, ceil
+
 def read_input(file_path):
     with open(file_path, 'r') as f:
         all_data = f.read()
@@ -17,10 +19,36 @@ def solve_a(pos):
     print('Total fuel cost is {0}'.format(total_fuel_cost))
 
 
+def calculate_fuel(positions, align_pos):
+    pos_diff = [abs(p - align_pos) for p in positions]
+    fuel_costs = [sum(range(p + 1)) for p in pos_diff]
+    return sum(fuel_costs)
+
+
+def solve_b(pos):
+    avg_pos = sum(pos) / len(pos)
+    avg_floor = floor(avg_pos)
+    avg_ceil = ceil(avg_pos)
+
+    fuel_floor = calculate_fuel(pos, avg_floor)
+    fuel_ceil = calculate_fuel(pos, avg_ceil)
+
+    print('Testing floor. Position {0}, fuel cost {1}'.format(avg_floor, fuel_floor))
+    print('Testing ceiling. Position {0}, fuel cost {1}'.format(avg_ceil, fuel_ceil))
+
+    min_fuel = min([fuel_floor, fuel_ceil])
+    
+    if min_fuel == fuel_floor:
+        min_pos = avg_floor
+    else:
+        min_pos = avg_ceil
+    print('The position {0} has the lowest fuel cost ({1}).'.format(min_pos, min_fuel))
+
+
 def main():
     positions = read_input('./input.txt')
-    # solve_a([16,1,2,0,4,2,7,1,2,14])
     solve_a(positions)
+    solve_b(positions)
 
 
 if __name__ == '__main__':
