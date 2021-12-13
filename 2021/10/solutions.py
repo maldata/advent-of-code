b396b84077536d8a890d56dd9720567a2705f81e
maldata <maldata@gmail.com>
2021-12-12 22:11:31 -0600

    Added 2021 day 10 part b in python

--- 2021/10/solutions.py
+++ 2021/10/solutions.py
@@ -18,17 +18,38 @@ def line_is_corrupt(line):
             if c != closers[most_recent_opener]:
                 return c
 
+    return char_stack
+
 
 def solve_a(all_lines):
     points = {')': 3, ']': 57, '}': 1197, '>': 25137}
     results = [line_is_corrupt(l) for l in all_lines]
-    errors = filter(lambda x: x is not None, results)
+    errors = filter(lambda x: isinstance(x, str), results)
     print('Total error score is {0}'.format(sum([points[e] for e in errors])))
 
 
-def solve_b(all_lines):
-    pass
+def calc_auto_score(remaining_stack):
+    points = {'(': 1, '[': 2, '{': 3, '<': 4}
+    multiplier = 5
+    score = 0
 
+    for char in remaining_stack[::-1]:
+        score = (score * multiplier) + points[char] 
+
+    return score
+
+
+def solve_b(all_lines):
+    results = [line_is_corrupt(l) for l in all_lines]
+    incompletes = filter(lambda x: isinstance(x, list), results)
+    scores = [calc_auto_score(i) for i in incompletes]
+    scores = [(scores[i], i) for i in range(len(scores))]
+    scores.sort()  # uses the first element of the tuple by default
+    num_incompletes = len(scores)
+
+    middle_idx = num_incompletes // 2
+    print('The middle score is {0}'.format(scores[middle_idx]))
+    
 
 def main():
     all_lines = read_input('./input.txt')
