1b82ffb29c01ac37f4e40b483a68067721eb5f64
maldata <maldata@gmail.com>
2022-02-06 22:33:42 -0600

    Added a good start at 2021 day 21 part b

--- 2021/21/solutions.py
+++ 2021/21/solutions.py
@@ -1,3 +1,4 @@
+from asyncio import all_tasks
 import re
 
 
@@ -48,13 +49,79 @@ def solve_a(positions):
         current_player_index = (current_player_index + 1) % num_players
 
     loser_score = scores[loser_index]
-    return loser_score * total_rolls
+    print('Result is {0}'.format(loser_score * total_rolls))
+
+
+def get_next_states_player(s, p):
+    if p < 0 or p > 1:
+        print('Bad player index {0}'.format(p))
+        return
+    
+    moving_player = p
+    static_player = 0 if p == 1 else 1
+    moving_player_state = s[moving_player]
+    static_player_state = s[static_player]
+
+    possible_next_states = []
+
+    # If the score of the moving player is already 21 or greater, we're done, no more moves.
+    if moving_player_state[1] >= 21:
+        return possible_next_states
+
+    num_spaces = 10
+    for roll in range(1,4):
+        new_pos = (((moving_player_state[0] - 1) + roll) % num_spaces) + 1
+        new_score = moving_player_state[1] + new_pos
+        
+        if moving_player == 0:
+            possible_next_states.append(((new_pos, new_score), static_player_state))
+        else:
+            possible_next_states.append((static_player_state, (new_pos, new_score)))
+
+    return possible_next_states
+
+
+def get_next_states(s):
+    ns0 = get_next_states_player(s, 0)
+    ns1 = get_next_states_player(s, 1)
+    return ns0 + ns1
+
+
+def generate_all_states(s0):
+    all_states = set()
+    all_states.add(s0)
+    states_to_check = set()
+    states_to_check.add(s0)
+
+    while len(states_to_check) != 0:
+        next_states = set()
+        for s in states_to_check:
+            all_states.add(s)
+            ns = get_next_states(s)
+            next_states.update(ns)
+
+        states_to_check = next_states - all_states
+
+    return all_states
+
+
+def solve_b(p1_pos, p2_pos):
+    # we'll call a "state" ((p1_pos, p1_score), (p2_pos, p2_score))
+    initial_state = ((p1_pos, 0), (p2_pos, 0))
+    all_states = generate_all_states(initial_state)
+    print('Found {0} states'.format(len(all_states)))
+
+    # We can just build the forward map by calling get_next_states()
+    # We can build the backward map kinda the same way
+    forward_map = {}   # a map from a state to all the states it could go to
+    backward_map = {}  # a map from a state to all the states that could've gotten us here
 
 
 def main():
     player1_pos, player2_pos = read_input('./input.txt')
-    result = solve_a([player1_pos, player2_pos])
-    print('Result is {0}'.format(result))
+    solve_a([player1_pos, player2_pos])
+    player1_pos, player2_pos = read_input('./input-test.txt')
+    solve_b(player1_pos, player2_pos)
 
 
 if __name__ == '__main__':
