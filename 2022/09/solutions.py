7507ffb86804762d3b61b95bd73e1c222a8503a1
Mark Locascio <mark.locascio@dmcinfo.com>
2022-12-21 12:36:37 -0600

    Finished 2022 day 09

--- 2022/09/solutions.py
+++ 2022/09/solutions.py
@@ -16,39 +16,43 @@ def update_head(head, direction):
     return (head[0] + dx, head[1] + dy)
 
 
-def update_tail(new_head, current_tail):
-    dx = new_head[0] - current_tail[0]
-    dy = new_head[1] - current_tail[1]
+def update_follower(updated_leader, current_follower):
+    dx = updated_leader[0] - current_follower[0]
+    dy = updated_leader[1] - current_follower[1]
 
+    # If it's 1 unit or fewer away in both x and y, then it doesn't move
     if abs(dx) <= 1 and abs(dy) <= 1:
-        return current_tail
+        return current_follower
 
-    if dx > 1:
-        dx = 1
-        dy = 0
-    elif dx < -1:
-        dx = -1
-        dy = 0
-    elif dy > 1:
-        dx = 0
-        dy = 1
-    elif dy < 1:
-        dx = 0
-        dy = -1
-    else:
-        print('Oof. Something weird.')
+    # If it's farther than that (i.e., if either dx or dy is >= 2), we need
+    # to move one space in any direction, including diagonals, to get closer. 
 
-    return (new_head[0] - dx, new_head[1] - dy)
-    
+    # Move one space closer along any axis for which dx or dy is > 0..
+    x_move = 0
+    y_move = 0
 
-def part1():
+    if dx > 0:
+        x_move = 1
+    elif dx < 0:
+        x_move = -1
+
+    if dy > 0:
+        y_move = 1
+    elif dy < 0:
+        y_move = -1
+
+    return (current_follower[0] + x_move, current_follower[1] + y_move)
+
+
+def simulate(num_segments):
     with open('input.txt', 'r') as f:
         all_lines = f.readlines()
 
-    head_pos = (0, 0)
-    tail_pos = (0, 0)
+    starting_pos = (0, 0)
+    body_segment_pos = [starting_pos] * num_segments
+
     visited = set()
-    visited.add(tail_pos)
+    visited.add(body_segment_pos[-1])
     
     for line in all_lines:
         line = line.strip()
@@ -56,22 +60,18 @@ def part1():
         direction = parts[0]
         steps = int(parts[1])
 
-        for step in range(steps):
-            new_head = update_head(head_pos, direction)
-            new_tail = update_tail(new_head, tail_pos)
+        for _ in range(steps):
+            body_segment_pos[0] = update_head(body_segment_pos[0], direction)
 
-            visited.add(new_tail)
+            for idx in range(1, num_segments):
+                leader = body_segment_pos[idx - 1]
+                body_segment_pos[idx] = update_follower(leader, body_segment_pos[idx])
 
-            head_pos = new_head
-            tail_pos = new_tail
+            visited.add(body_segment_pos[-1])
 
     print(f'The tail visited {len(visited)} places')
 
 
-def part2():
-    pass
-
-
 if __name__ == '__main__':
-    part1()
-    part2()
+    simulate(2)
+    simulate(10)
