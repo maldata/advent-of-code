36a1160ed93f64d22b7445e54473071729d04007
maldata <maldata@gmail.com>
2023-12-19 22:29:20 -0600

    Finished 2023 day 1 part 2

--- 2023/01/part1.py
+++ 2023/01/part1.py
@@ -1,16 +1,44 @@
+def get_indices_of_numerals(full_line):
+    exploded_line = [i for i in full_line]
+    numbers = filter(lambda x: x[1].isnumeric(), enumerate(exploded_line))
+    return [(p[0], int(p[1])) for p in numbers]
+
+
 def part1():
     with open('./input.txt', 'r') as f:
         all_lines = f.readlines()
     
     all_lines = [l.strip() for l in all_lines]
-    exploded_lines = [[i for i in l] for l in all_lines]
-    numbers_only = []
-    for exploded_line in exploded_lines:
-        numbers = filter(lambda x: x.isnumeric(), exploded_line)
-        numbers_only.append(list(numbers))
-    
-    calibration_values = [int(n[0] + n[-1]) for n in numbers_only]
+    firsts_and_lasts = []
+    for line in all_lines:
+        indices_and_values = get_indices_of_numerals(line)
+        indices_and_values.sort(key=lambda x: x[0])
+        values = [i[1] for i in indices_and_values]
+        firsts_and_lasts.append( (values[0], values[-1]) )
+
+    calibration_values = [(10 * i[0]) + i[1] for i in firsts_and_lasts]
     print(f'Part 1: {sum(calibration_values)}')
+
+
+def get_indices_of_words(full_line):
+    indices_and_values = []
+    for text_num in (('one', 1), ('two', 2), ('three', 3), 
+                     ('four', 4), ('five', 5), ('six', 6),
+                     ('seven', 7), ('eight', 8), ('nine', 9)):
+        idx = 0
+        while True:
+            found_idx = full_line.find(text_num[0], idx)
+            if found_idx < 0:
+                break
+            else:
+                indices_and_values.append((found_idx, text_num[1]))
+                idx = found_idx + 1
+
+    return indices_and_values
+
+
+def get_indices_and_values(full_line):
+    return get_indices_of_numerals(full_line) + get_indices_of_words(full_line)
 
 
 def part2():
@@ -18,26 +46,14 @@ def part2():
         all_lines = f.readlines()
     
     all_lines = [l.strip() for l in all_lines]
-    calibration_values = []
+    firsts_and_lasts = []
     for line in all_lines:
-        exploded_line = [i for i in line]
-        numbers = filter(lambda x: x[1].isnumeric(), enumerate(exploded_line))
-        indices_and_values = list(numbers)
-        indices_and_values = [(p[0], int(p[1])) for p in indices_and_values]
-
-        # TODO: line 16 has two instances of "two" in it... we need to find both!
-
-        for text_num in (('one', 1), ('two', 2), ('three', 3), 
-                         ('four', 4), ('five', 5), ('six', 6),
-                         ('seven', 7), ('eight', 8), ('nine', 9)):
-            index = line.find(text_num[0])
-            if index >= 0:
-                indices_and_values.append((index, text_num[1]))
-   
+        indices_and_values = get_indices_and_values(line)
         indices_and_values.sort(key=lambda x: x[0])
         values = [i[1] for i in indices_and_values]
-        calibration_values.append(values[0] + values[-1])
-    
+        firsts_and_lasts.append( (values[0], values[-1]) )
+
+    calibration_values = [(10 * i[0]) + i[1] for i in firsts_and_lasts]    
     print(f'Part 2: {sum(calibration_values)}')
 
 
